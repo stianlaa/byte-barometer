19424b6b24c61c7f37c15b5be90e60fb089e73ab
Stian Laate <stian@sparkel.io>
2023-06-26 14:45:37 +0200

    Add aggregate sentiment

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,6 @@
 import * as dotenv from "dotenv";
 import { fetchComments } from "./hackernews-client.js";
-import { inferSentiment } from "./sentiment-client.js";
+import { awaitModelLoaded, inferSentiment } from "./sentiment-client.js";
 import { getCliArguments } from "./util.js";
 
 dotenv.config({ override: true });
@@ -9,15 +9,27 @@ export const run = async () => {
   // Get current run arguments
   const { query, commentCount } = getCliArguments();
 
+  // Ensure model is ready
+  await awaitModelLoaded();
+
   // Search Hacker News for the relevant comments since last year
   const fromSeconds = Math.floor(Date.now() / 1000) - 31536000;
   const comments = await fetchComments(query, commentCount, fromSeconds);
 
   // Infer the sentiment of the comments with regards to the query
-  const inferredSentimentArray = await inferSentiment(comments);
+  const inferredSentimentArray = await inferSentiment(query, comments);
 
-  for (let sentinment of inferredSentimentArray) {
-    console.log(sentinment.comment_text, sentinment.sentiment, "\n");
+  // Sort the inferredSentimentArray by sentiment score, 1 to -1 (positive to negative)
+  const sortedSentiment = inferredSentimentArray.sort(
+    (a, b) => a.positive - a.negative - (b.positive - b.negative)
+  );
+
+  for (const sentinment of sortedSentiment) {
+    console.log(
+      sentinment.comment_text,
+      sentinment.positive - sentinment.negative,
+      "\n"
+    );
   }
 };
 
--- src/sentiment-client.ts
+++ src/sentiment-client.ts
@@ -5,30 +5,78 @@ import { Comment } from "./hackernews-client.js";
 const MODEL = "yangheng/deberta-v3-large-absa-v1.1";
 const HUGGING_FACE_URL = "https://api-inference.huggingface.co/";
 
+const POSITIVE = "Positive";
+const NEGATIVE = "Negative";
+
 type Sentiment = {
   label: string;
   score: number;
 };
 
-type InferredSentiment = {
+type RawSentiment = {
   comment_text: string;
   sentiment: Sentiment[];
 };
 
-export const inferSentiment = async (comments: Comment[]) => {
-  const inferredSentimentArray: InferredSentiment[] = [];
-  for (const comment of comments) {
-    const { data } = await axios.post(
-      `${HUGGING_FACE_URL}models/${MODEL}`,
-      comment.comment_text,
-      {
+type InferredSentiment = {
+  comment_text: string;
+  positive: number;
+  negative: number;
+  neutral: number;
+};
+
+// Await huggingface model loaded
+// TODO improve to actually make sense
+export const awaitModelLoaded = async () => {
+  for (let retries = 0; retries < 5; retries++) {
+    try {
+      await axios.get(`${HUGGING_FACE_URL}models/${MODEL}`, {
         headers: { Authorization: `Bearer ${getEnv("HUGGINGFACE_API_KEY")}` },
-      }
-    );
-    inferredSentimentArray.push({
+      });
+      return;
+    } catch (error) {
+      console.log("Should check 503 and retry");
+      // Wait for 3 seconds
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+    }
+  }
+};
+
+export const inferSentiment = async (query: string, comments: Comment[]) => {
+  const inferredSentimentArray: InferredSentiment[] = [];
+  const inputs = comments.map(
+    (comment) => `[CLS] ${comment.comment_text} [SEP] ${query} [SEP]`
+  );
+
+  // Time request
+  console.time("Infer sentiment");
+  const { data } = await axios.post(
+    `${HUGGING_FACE_URL}models/${MODEL}`,
+    inputs,
+    {
+      headers: { Authorization: `Bearer ${getEnv("HUGGINGFACE_API_KEY")}` },
+    }
+  );
+  console.timeEnd("Infer sentiment");
+
+  // Enumerate the comments and their inferred sentiment
+  for (const [index, comment] of comments.entries()) {
+    let inferredSentiment = {
       comment_text: comment.comment_text,
-      sentiment: data[0], // TODO add batch support somehow?
-    });
+      positive: 0,
+      negative: 0,
+      neutral: 0,
+    };
+    for (const sentiment of data[index]) {
+      if (sentiment.label === POSITIVE) {
+        inferredSentiment.positive = sentiment.score;
+      } else if (sentiment.label === NEGATIVE) {
+        inferredSentiment.negative = sentiment.score;
+      } else {
+        inferredSentiment.neutral = sentiment.score;
+      }
+    }
+    inferredSentimentArray.push(inferredSentiment);
   }
   return inferredSentimentArray;
 };
