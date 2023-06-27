e4ffc3ecae566d7d37664f8ca5ab17ffa8bc0982
Stian Laate <stian@sparkel.io>
2023-06-27 11:23:07 +0200

    Add initial layout

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,24 @@
+import { Box, SimpleGrid } from "@chakra-ui/react";
 import "./App.css";
+import "./index.css";
 
 function App() {
-  return <h1>Byte barometer</h1>;
+  return (
+    <Box w="100%">
+      <Box w="100%" h="100px" bg="blue.500">
+        Wide Element
+      </Box>
+
+      <SimpleGrid columns={2} spacing={10}>
+        <Box h="100px" bg="green.500">
+          Element 1
+        </Box>
+        <Box h="100px" bg="red.500">
+          Element 2
+        </Box>
+      </SimpleGrid>
+    </Box>
+  );
 }
 
 export default App;
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,10 +1,23 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+// 2. Extend the theme to include custom colors, fonts, etc
+const colors = {
+  brand: {
+    900: "#1a365d",
+    800: "#153e75",
+    700: "#2a69ac",
+  },
+};
+
+const theme = extendTheme({ colors });
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <ChakraProvider theme={theme}>
+      <App />
+    </ChakraProvider>
+  </React.StrictMode>
+);
