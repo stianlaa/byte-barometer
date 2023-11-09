5dfbb51f3d56a24b7ecdcad98312bde06053ff7c
Stian Laate <stian@sparkel.io>
2023-11-09 12:40:28 +0100

    Handle exceptions and unavailability gently in backend client

--- watchdog/backend_client.py
+++ watchdog/backend_client.py
@@ -6,5 +6,9 @@ from logger_setup import logger
 PAPERSPACE_BACKEND_URL = environ.get("PAPERSPACE_BACKEND_URL", "http://localhost:3000") 
 
 def checkin_with_backend() -> bool:
-    response = requests.get(f"{PAPERSPACE_BACKEND_URL}/checkin")
-    return response.status_code == 200
+    try:
+        response = requests.get(f"{PAPERSPACE_BACKEND_URL}/checkin")
+        return response.status_code == 200
+    except requests.exceptions.RequestException as e:
+        logger.info(f"Checkin returned not-ok status: {e}")
+        return False
