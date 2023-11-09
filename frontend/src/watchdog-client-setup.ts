e62045cc84c3877e2f4054a20980bed8f37a10c1
Stian Laate <stian@sparkel.io>
2023-11-09 11:52:29 +0100

    Adjust watchdog url

--- frontend/src/watchdog-client-setup.ts
+++ frontend/src/watchdog-client-setup.ts
@@ -2,6 +2,6 @@ const ENV = process.env.NODE_ENV;
 
 const DEV_URL = "http://localhost:2999";
 const PRODUCTION_URL =
-  "https://dea6478627e3f46ecb2eeefe0e841eb8f.clg07azjl.paperspacegradient.com";
+  "https://d93c90d2d85184abc88b57517a180d6b8.clg07azjl.paperspacegradient.com";
 
 export const WATCHDOG_URL = ENV === "development" ? DEV_URL : PRODUCTION_URL;
